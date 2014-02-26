592e79375eafb21f753359a7aa5056633ade6d69
Maurice Fonk <git@naneau.net>
2014-02-26 16:27:33 -0500

    adding test to make sure text gets rendered inside html5 tag shorthand

--- test/coffee/tagsTest.coffee
+++ test/coffee/tagsTest.coffee
@@ -12,5 +12,6 @@ test 'tags', ->
 
   equal wrapper.children().length, 3, 'Root elements should render in correct number'
   equal wrapper.children()[0].tagName, 'DIV', 'Element should render as correct type'
+  equal wrapper.children()[0].innerHTML, 'foo', 'Element should get text'
   equal wrapper.children()[1].tagName, 'P', 'Element should render as correct type'
   equal wrapper.children()[2].tagName, 'A', 'Element should render as correct type'
--- test/js/tagsTest.js
+++ test/js/tagsTest.js
@@ -10,6 +10,7 @@
     }));
     equal(wrapper.children().length, 3, 'Root elements should render in correct number');
     equal(wrapper.children()[0].tagName, 'DIV', 'Element should render as correct type');
+    equal(wrapper.children()[0].innerHTML, 'foo', 'Element should get text');
     equal(wrapper.children()[1].tagName, 'P', 'Element should render as correct type');
     return equal(wrapper.children()[2].tagName, 'A', 'Element should render as correct type');
   });
